a149c9f4d1b3c082692562489ec9b9b9032414f1
Sara Bee <sara.bainbridge@gmail.com>
2016-02-24 10:08:17 -0500

    Commit generated go templates

--- dist/go/slack/check-failing.go
+++ dist/go/slack/check-failing.go
@@ -8,7 +8,7 @@ var CheckFailing = `{
     {
       "pretext": "Failing check",
       "title": "{{check_name}} failing in {{group_name}}",
-      "title_link": "https://app.opsee.com/check/{{check_id}}",
+      "title_link": "https://app.opsee.com/check/{{check_id}}/event?json={{json_url}}",
       "text": "{{fail_count}} of {{instance_count}} Failing",
       "color": "#f44336"
     }
--- dist/go/slack/check-passing.go
+++ dist/go/slack/check-passing.go
@@ -8,7 +8,7 @@ var CheckPassing = `{
     {
       "pretext": "Passing check",
       "title": "{{check_name}} passing in {{group_name}}",
-      "title_link": "https://app.opsee.com/check/{{check_id}}",
+      "title_link": "https://app.opsee.com/check/{{check_id}}/event?json={{json_url}}",
       "text": "{{instance_count}} Passing",
       "color": "#69a92c"
     }
