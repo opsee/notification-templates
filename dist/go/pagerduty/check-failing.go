c8437d9331b5650db2f7835ea575a33b7ed2c259
Dan Compton <dan@opsee.com>
2016-05-03 17:08:31 -0400

    Fix pagerduty json_url

--- dist/go/pagerduty/check-failing.go
+++ dist/go/pagerduty/check-failing.go
@@ -3,7 +3,7 @@ var CheckFailing = `{
   "service_key": "{{service_key}}",
   "incident_key":"{{check_id}}",
   "description":"{{check_name}} failure in {{group_name}}",
-  "client_url":"https://{{opsee_host}}/check/{{check_id}}/event?json={{json_url}}",
+  "client_url":"https://{{opsee_host}}/check/{{check_id}}",
   "event_type":"trigger",
   "details":"{{{event_json}}}"
 }
