b3fa3895f6536330d81ade6c412c5640bcc160c1
Corey Light <coreylight@gmail.com>
2016-08-08 10:49:48 -0500

    new event url scheme

--- dist/go/pagerduty/check-failing.go
+++ dist/go/pagerduty/check-failing.go
@@ -6,7 +6,7 @@ var CheckFailing = `{
   "service_key": "{{service_key}}",
   "incident_key":"{{check_id}}",
   "description":"{{check_name}} failure in {{group_name}}",
-  "client_url":"https://{{opsee_host}}/check/{{check_id}}",
+  "client_url":"https://{{opsee_host}}/check/{{check_id}}/{{event_id}}",
   "event_type":"trigger",
   "details":"{{{event_json}}}"
 }
